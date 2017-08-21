3682364c8e894ea66038ccc3e2a30dc60ff616c1
morgan.smith <msmith1270@augusta.edu>
2017-08-21 10:37:35 -0400

    created game class, changed some registration stuff (need to add controls), some little design stuff(make pretty)

---
+++ gamestation/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gamestation.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCheckedOut { get; set; }
+    }
+}
