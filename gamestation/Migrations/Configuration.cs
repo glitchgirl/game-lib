e7b97cd709392ede26c59c632bbd672288cb0ea2
morgan.smith <msmith1270@augusta.edu>
2017-08-24 16:54:28 -0400

    played with data, not much progress

--- gamestation/Migrations/Configuration.cs
+++ gamestation/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace gamestation.Migrations
 {
+    using Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -15,18 +17,7 @@ namespace gamestation.Migrations
 
         protected override void Seed(gamestation.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+         
         }
     }
 }
